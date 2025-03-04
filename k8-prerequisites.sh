52d986f94459fb1a57ab226e868c2206adfc46a5
spli5nabeel <spli5nabeel@gmail.com>
2025-03-04 14:43:27 +0000

    Revert "test 4" to "test 3"
    This reverts commit 0844828c74c05d0536615a48f52083b206fc1e8c.

--- k8-prerequisites.sh
+++ k8-prerequisites.sh
@@ -6,7 +6,7 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-## sysctl params required by setup, params persist across reboots
+# sysctl params required by setup, params persist across reboots
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
